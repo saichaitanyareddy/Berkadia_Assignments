55e9f6127da37417f881a301ae782041d216cc24
Saichaitanya Reddy <saichaitanya338@gmail.com>
2018-11-30 12:43:15 +0530

    Update 2) Inheritance.js

--- Nodejs/Week_3/2) Inheritance.js
+++ Nodejs/Week_3/2) Inheritance.js
@@ -1,6 +1,4 @@
-Inheritance:Inheritance is the property of accquiring attributes and operations from parent along with it's own properties.
-
-
+Inheritance: Inheritance is the property of accquiring attributes and operations from parent along with it's own properties.
 class A
 {
 	A(name){
